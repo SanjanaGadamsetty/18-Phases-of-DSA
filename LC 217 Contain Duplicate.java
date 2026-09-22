d7724c8b6d581fea2797b6041ca3013734c527fc
Gadamsetty Venkata Nagalakshmi Sanjana <gvnsanjana.ds@gmail.com>
2026-09-22 22:16:11 +0530

    Implement containsDuplicate method in Solution class

---
+++ LC 217 Contain Duplicate.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int n : nums) numSet.add(n);
+        return numSet.size() < nums.length;
+    }
+}
