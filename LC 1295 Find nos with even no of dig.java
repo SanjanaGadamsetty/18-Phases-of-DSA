dd6e1bec0cb175f284e517fdbd028a40020118d3
Gadamsetty Venkata Nagalakshmi Sanjana <gvnsanjana.ds@gmail.com>
2026-09-22 21:30:09 +0530

    Add solution to count numbers with even digits

---
+++ LC 1295 Find nos with even no of dig.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int cnt = 0;
+        for (int n : nums){
+            int dig = (int) Math.log10(n) + 1;
+            if (dig % 2 == 0) cnt++;
+        }
+        return cnt;
+    }
+}
